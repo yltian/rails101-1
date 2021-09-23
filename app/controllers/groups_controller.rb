52782459979573cfa13d7578aa962686abf2e05f
yltian <54483391@qq.com>
2021-09-23 22:05:32 +0800

    implement groups#update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -26,6 +26,11 @@ class GroupsController < ApplicationController
   end
 
   def update
+    @group = Group.find(params[:id])
+
+    @group.update(group_params)
+
+    redirect_to groups_path, notice: "Update Success"
   end
 
   def destroy
