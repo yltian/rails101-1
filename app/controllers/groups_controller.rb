0e6f634667a91a5755898951ec59ca2114bda0ef
yltian <54483391@qq.com>
2021-09-23 21:48:02 +0800

    implement groups#create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,4 +6,32 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
   end
+
+  def create
+    @group = Group.new(group_params)
+
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
 end
