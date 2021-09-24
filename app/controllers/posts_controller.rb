9d0db84eeebf81ebc4da668eb206f8eb7f6cab00
yltian <54483391@qq.com>
2021-09-25 02:58:56 +0800

    implement post 'edit/destroy' action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, :only => [:new, :create]
+  before_action :find_group_post, only: [:edit, :update, :destroy]
 
   def new
     @group = Group.find(params[:group_id])
@@ -20,9 +21,32 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to group_path(@group)
+      flash[:notice] = "update post success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    flash[:warning] = "destroy the post"
+    redirect_to group_path(@group)
+  end
+
   private
   def post_params
     params.require(:post).permit(:content)
+  end
+
+  def find_group_post
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.find(params[:id])
   end
 
 
